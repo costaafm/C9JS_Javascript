6ac430a404dedede3eedff74e9703692fcf261cc
André Costa <costaafm@gmail.com>
2016-02-24 16:09:57 +0000

    Object_Literals

---
+++ script12.js
@@ -0,0 +1,68 @@
+//defining a object structure
+var batwing= {
+	//propertie
+	status:"Ready",
+	//Method
+	rescueBatman: function(){
+		document.write("Locating his transponder...initiating launch...");
+	}
+}
+/*
+if (batwing.status==="Ready") {
+	batwing.rescueBatman();
+}
+*/
+
+
+var utilities={
+	
+	printAllMembers: function(targetObject) {
+		for (i  in targetObject){
+			document.write("<br/>" + targetObject[i]);
+		}
+	}
+}
+
+//utilities.printAllMembers(batwing);
+
+/*
+// Empty object literal
+var i_am_empty ={ };
+utilities.printAllMembers(i_am_empty);
+*/
+
+var planet ={
+	id:34,
+	name: "IO",
+	faction:{
+		factiondId:2,
+		name: "Nex",
+		notification: function(){
+			document.write("Nex alliance...unite!");
+		}
+	},
+	cities: [ 
+	{ locationId: 15,name: "gladius"},
+		{locationId:16,name:"Chalbys"},
+		{locationId:17,name:"Ensis"},
+		]	
+};
+
+//planet.faction.notification();
+
+//document.write(planet.cities[1].name);
+/*
+document.write("<br/>"+planet.name);// global variable
+planet.name="Vultus";
+document.write("<br/>"+planet.name); // local variable
+*/
+
+/*
+var z=planet;
+document.write(z.name);
+*/
+
+if (typeof planet.defense==="undefined") {
+	planet.defense="Ion Cannon";
+}
+document.write(planet.defense);
